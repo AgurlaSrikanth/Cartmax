33439a4384eee9dfc05e00e590f7171ed35c03cb
AakashManocha <57034052+AakashManocha@users.noreply.github.com>
2022-06-22 11:18:35 +0530

    commit by Aakash

---
+++ buynow/buynow/src/main/java/com/cartmax/buynow/controller/cartmaxcontroller.java
@@ -0,0 +1,198 @@
+package com.cartmax.buynow.controller;
+import org.apache.tomcat.jni.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class cartmaxcontroller {
+	
+//	@Autowired
+//	 private JpaRepositoryExtension userRepo;
+	 
+	     @GetMapping("")
+	     public String viewHomePage1() {
+	    	 return "Login";
+	     }
+	     @GetMapping("/Home")
+	     public String Home() {
+	    	 return "home";
+	     }
+		@GetMapping("/categories")
+			public String listCategories() {
+			return "Categories";
+		}
+		@GetMapping("/mens")
+		public String listmens() {
+		return "mens";
+	}
+		@GetMapping("/womens")
+		public String listwomens() {
+		return "womens";
+	}
+		
+		@GetMapping("/kids")
+		public String listkids() {
+		return "kids";
+	}
+		
+		@GetMapping("/cosmetics")
+		public String listcosmetics() {
+		return "cosmetics";
+	}
+		
+		@GetMapping("/slipper")
+		public String listslipper() {
+		return "slipper";
+	}
+		@GetMapping("/payment")
+		public String listpayment() {
+		return "payment";
+	}
+		
+		@GetMapping("/delivery")
+		public String listdelivery() {
+		return "delivery";
+	}
+		@GetMapping("/cart")
+		public String listcart() {
+		return "cart";
+	}
+		@GetMapping("/contact")
+		public String listcontact() {
+		return "contact";
+	}
+		
+		
+		@GetMapping("/Aboutus")
+		public String listAboutus() {
+		return "aboutus";
+	}
+		@GetMapping("/terms")
+		public String listterms() {
+		return "terms";
+	}
+		@GetMapping("/order")
+		public String listorder() {
+		return "order";
+	}
+		
+		@GetMapping("/admin")
+		public String listadmin() {
+		return "admin";
+	}
+		
+		@GetMapping("/signupnow")
+		public String listsignupnow() {
+		return "signupnow";
+	}	
+		
+		
+		@GetMapping("/resetpswd")
+		public String listresetpswd() {
+		return "resetpswd";
+	}	
+		
+		@GetMapping("/signin")
+		public String showLoginPage() {
+		return "signin";
+
+
+
+
+		}
+		
+
+
+
+		@GetMapping("/about")
+		public String listAbout() {
+		return "about";
+
+
+
+		}
+		@GetMapping("/index")
+		public String index() {
+		return "index";
+
+
+
+		}
+
+
+
+		@GetMapping("/checkout")
+		public String checkout() {
+		return "checkout";
+		}
+
+
+
+		@GetMapping("/products")
+		public String products() {
+		return "products";
+		}
+		
+		
+		
+		
+		@RequestMapping("/login")
+		public ModelAndView welcome() {
+			System.out.println("welcome home");
+			ModelAndView modelAndView = new ModelAndView();
+			modelAndView.setViewName("login_form.html");
+			return modelAndView;
+		}
+
+		
+	//@GetMapping("/login")
+		public String showLoginForm(Model model) {
+			model.addAttribute("user", new org.apache.tomcat.jni.User());
+			System.out.println("calling login page");
+			return "login_form";
+		}
+
+		//@GetMapping("/process_login")
+		public String login(@RequestBody org.apache.catalina.User user) {
+			System.out.println("calling process_login ");
+//			return cartmaxService.login(user);
+			return "success";
+		}
+
+		@GetMapping("/register")
+		public String showRegistrationForm(Model model) {
+			model.addAttribute("user", new org.apache.tomcat.jni.User());
+			System.out.println("calling register page");
+			return "signup_form.html";
+		}
+
+		@PostMapping("/process_register")
+		public String processRegister(org.apache.catalina.User user) {
+
+//		    userRepo.save(user);
+			System.out.println("calling process register page");
+			return "register_success";
+		}
+
+
+	//@GetMapping("/login")
+	public ModelAndView welcome1() {
+		System.out.println("welcome home");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login_form.html");
+		return modelAndView;
+	}
+
+}
+
+
+
